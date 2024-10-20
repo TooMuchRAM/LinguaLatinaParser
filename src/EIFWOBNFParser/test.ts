bba593990e6958fb32339aaa8c376ef23e97e000
Thijs <me@toomuchram.net>
2024-10-20 10:12:09 +0200

    Remove console.log

--- src/EIFWOBNFParser/test.ts
+++ src/EIFWOBNFParser/test.ts
@@ -3,7 +3,6 @@ import LinguaLatinaParser from "../LinguaLatinaParser";
 const llParser = new LinguaLatinaParser();
 llParser.parse("discipuli hominem parvum abutere dicunt").then(
     (result) => {
-        console.log(llParser);
         console.log(result);
     }
 );
